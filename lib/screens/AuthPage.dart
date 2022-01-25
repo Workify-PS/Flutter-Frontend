677269037cd59ab3eed7d155d32f281b43673a2a
Samarth Khanna <samarth.khanna@peoplestrong.com>
2022-01-25 19:03:08 +0530

    Login Screen: Dummy Page Ready

--- lib/screens/AuthPage.dart
+++ lib/screens/AuthPage.dart
@@ -1,133 +1,245 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:workify/utils/sizes.dart';
 
-class AuthPage extends StatelessWidget {
+class AuthPage extends StatefulWidget {
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  bool _passwordIsVisible = false;
   @override
   Widget build(BuildContext context) {
+    DeviceSize device = DeviceSize();
+    device.size = MediaQuery.of(context).size;
+
     return Container(
       decoration: const BoxDecoration(
           gradient: LinearGradient(
         begin: Alignment.topLeft,
         end: Alignment.bottomRight,
-        colors: [Colors.indigo, Colors.teal],
+        colors: [Color(0xff2082ff), Colors.teal],
       )),
       child: Scaffold(
           backgroundColor: Colors.transparent,
-          body: SingleChildScrollView(
-              child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Image.asset(
-                'assets/images/banner512.png',
-              ),
-              Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const SizedBox(
-                        height: 30,
+          body: Center(
+            child: Container(
+              height: device.size.height / 1.3,
+              width: device.size.width / 1.2,
+              alignment: Alignment.center,
+              child: Card(
+                elevation: 100,
+                color: Colors.grey.shade300,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Expanded(
+                      flex: 9,
+                      child: Image.asset(
+                        'assets/images/banner512_nobg.png',
                       ),
-                      Text(
-                        'LOGIN',
-                        textAlign: TextAlign.center,
-                        style: GoogleFonts.lato(
-                          textStyle: const TextStyle(
-                            fontSize: 35,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        'Please Login to Your Account',
-                        textAlign: TextAlign.center,
-                        style: GoogleFonts.lato(
-                          textStyle: const TextStyle(
-                            color: Colors.grey,
-                            fontSize: 15,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      const SizedBox(
-                        width: 250,
-                        child: TextField(
-                          decoration: InputDecoration(
-                            labelText: 'Email Address',
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 250,
-                        child: TextField(
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            labelText: 'Password',
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(20, 20, 115, 20),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: const [
-                            Text(
-                              'Forget Password',
-                              style: TextStyle(color: Colors.blue),
-                            )
+                    ),
+                    Expanded(
+                      flex: 10,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Expanded(
+                              flex: 2,
+                              child: Padding(
+                                padding: const EdgeInsets.only(
+                                    top: 24.0, bottom: 16),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.max,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      'LOGIN',
+                                      textAlign: TextAlign.center,
+                                      style: GoogleFonts.lato(
+                                        textStyle: const TextStyle(
+                                          fontSize: 35,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                    Text(
+                                      'Please Login to Your Account',
+                                      textAlign: TextAlign.center,
+                                      style: GoogleFonts.lato(
+                                        textStyle: const TextStyle(
+                                          color: Colors.grey,
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              flex: 3,
+                              child: Container(
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 50),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.max,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Padding(
+                                      padding:
+                                          const EdgeInsets.only(bottom: 4.0),
+                                      child: TextFormField(
+                                        keyboardType:
+                                            TextInputType.emailAddress,
+                                        decoration: InputDecoration(
+                                          labelText: 'User ID',
+                                          border: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(8.0),
+                                            borderSide: BorderSide(
+                                                color: Colors.black, width: 2),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(top: 4.0),
+                                      child: TextFormField(
+                                        obscureText: !_passwordIsVisible,
+                                        decoration: InputDecoration(
+                                          suffixIcon: IconButton(
+                                              onPressed: () {
+                                                setState(() {
+                                                  _passwordIsVisible =
+                                                      !_passwordIsVisible;
+                                                });
+                                              },
+                                              icon: Icon(
+                                                _passwordIsVisible
+                                                    ? Icons.visibility_sharp
+                                                    : Icons
+                                                        .visibility_off_outlined,
+                                                color: Colors.black,
+                                              )),
+                                          labelText: 'Password',
+                                          border: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(8.0),
+                                            borderSide: BorderSide(
+                                                color: Colors.black, width: 2),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: [
+                                        InkWell(
+                                          onTap: () {},
+                                          child: Padding(
+                                            padding: const EdgeInsets.only(
+                                                right: 8.0, top: 4),
+                                            child: Text(
+                                              'Forgot Password ?',
+                                              textAlign: TextAlign.right,
+                                              style: TextStyle(
+                                                  color: Colors.blue,
+                                                  fontSize: 12),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              flex: 3,
+                              child: Column(
+                                mainAxisSize: MainAxisSize.max,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  InkWell(
+                                    onTap: () {},
+                                    child: Container(
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 12),
+                                      alignment: Alignment.center,
+                                      margin: const EdgeInsets.symmetric(
+                                          horizontal: 120),
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(8),
+                                          color:
+                                              Theme.of(context).primaryColor),
+                                      child: Text(
+                                        'Login',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 16,
+                                          //fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding:
+                                        const EdgeInsets.symmetric(vertical: 4),
+                                    child: Text(
+                                      'or',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                  InkWell(
+                                    onTap: () {},
+                                    child: ElevatedButton(
+                                      onPressed: () {},
+                                      style: ElevatedButton.styleFrom(
+                                          primary: Colors.grey.shade50,
+                                          onPrimary: Colors.black),
+                                      child: Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            vertical: 12),
+                                        child: Wrap(
+                                          crossAxisAlignment:
+                                              WrapCrossAlignment.center,
+                                          children: [
+                                            Icon(
+                                              Icons.android,
+                                              color: Colors.greenAccent,
+                                            ),
+                                            SizedBox(width: 12),
+                                            Text('Sign in with Google'),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
                           ],
                         ),
                       ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      GestureDetector(
-                        child: Container(
-                          alignment: Alignment.center,
-                          width: 250,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(50),
-                              gradient: const LinearGradient(
-                                  begin: Alignment.centerLeft,
-                                  end: Alignment.centerRight,
-                                  colors: [Colors.indigo, Colors.teal])),
-                          child: const Padding(
-                            padding: EdgeInsets.all(15.0),
-                            child: Text(
-                              'Login',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      const Text(
-                        'Or Login Using Google',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  ),
+                    )
+                  ],
                 ),
-              )
-            ],
-          ))),
+              ),
+            ),
+          )),
     );
   }
 }

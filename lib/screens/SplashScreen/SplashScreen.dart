5f8b33c065e0be9ba094ac1d0f59ed0188951008
ajayg51 <ajay.cs18@nitp.ac.in>
2022-02-17 21:39:24 +0530

    MISTAKE :: SplashScreen.dart file was not staged

--- lib/screens/SplashScreen/SplashScreen.dart
+++ lib/screens/SplashScreen/SplashScreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:workify/controllers/AuthController.dart';
 import 'package:workify/controllers/UserController.dart';
+import 'package:workify/exceptions/print_log.dart';
 import 'package:workify/screens/SplashScreen/OnBoardingScreen.dart';
 import 'package:workify/screens/SplashScreen/splash_widget.dart';
 
@@ -9,22 +10,27 @@ class SplashScreen extends GetView<AuthController> {
   const SplashScreen({Key? key}) : super(key: key);
 
   Future<void> initializeSettings() async {
-     //controller.checkLoginStatus();
-      if (controller.isSignedIn.value) {
-        print("USER IS SIGNED IN");
-        final token = controller.getToken();
-        print("WITH TOKEN $token");
-        final _userController = Get.find<UserController>();
-        await _userController.setUser(token!);
-        
-        Get.toNamed("/home");
-      } else {
-        print(
-            '\n-- In SplashScreen.dart file :: initializeSettings(){} : Block 1\n');
-        print("CAN'T SET USER, NOT SIGNED IN");
-        print('-------------- End Block 1 ---------------');
-        Get.toNamed("/auth");
-      }
+    //controller.checkLoginStatus();
+    if (controller.isSignedIn.value) {
+      final token = controller.getToken();
+      final _userController = Get.find<UserController>();
+      await _userController.setUser(token!);
+      Get.toNamed("/home");
+      PrintLog.printLog(
+        fileName:'SplashScreen',
+        functionName: 'initializeSettings',
+        blockNumber: 1,
+        printStatement: 'User is logged in with Token\n$token',
+      );
+    } else {
+      PrintLog.printLog(
+        fileName:'SplashScreen',
+        functionName: 'initializeSettings',
+        blockNumber: 2,
+        printStatement: 'User is not logged In !!',
+      );
+      Get.toNamed("/auth");
+    }
   }
 
   @override

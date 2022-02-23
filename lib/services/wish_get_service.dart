73f0af8509afc6bfb691bea36eaaccd61b28f538
Samarth Khanna <samarth.khanna@peoplestrong.com>
2022-02-24 03:27:04 +0530

    Added urls for all wish services

--- lib/services/wish_get_service.dart
+++ lib/services/wish_get_service.dart
@@ -3,10 +3,12 @@ import 'package:get/get_connect/http/src/status/http_status.dart';
 import 'package:workify/models/WishModel.dart';
 
 class WishGetService extends GetConnect {
-  final String wishGetURL = "http://localhost:8080/workify/v1/wish/birthday";
+  final String bdayURL = "http://localhost:8080/workify/v1/wish/birthday";
+  final String annURL = "http://localhost:8080/workify/v1/wish/birthday";
+  final String newJURL = "http://localhost:8080/workify/v1/wish/birthday";
   Future<List<WishModel>?> getBirthday() async {
     var headers = {'Content-Type': 'application/json'};
-    final response = await get(wishGetURL, headers: headers);
+    final response = await get(bdayURL, headers: headers);
 
     if (response.statusCode == HttpStatus.ok) {
       List<dynamic> bdays = response.body;

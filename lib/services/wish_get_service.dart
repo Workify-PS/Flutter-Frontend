55f4413bde3be454e852255f8fb17a7ea48247c5
Samarth Khanna <samarth.khanna@peoplestrong.com>
2022-02-24 03:17:42 +0530

    Added Wish controller via Getx

--- lib/services/wish_get_service.dart
+++ lib/services/wish_get_service.dart
@@ -4,32 +4,15 @@ import 'package:workify/models/WishModel.dart';
 
 class WishGetService extends GetConnect {
   final String wishGetURL = "http://localhost:8080/workify/v1/wish/birthday";
-  Future<WishModel?> wishGetService() async {
+  Future<List<WishModel>?> getBirthday() async {
     var headers = {'Content-Type': 'application/json'};
+    final response = await get(wishGetURL, headers: headers);
 
-    try {
-      final response = await get(wishGetURL, headers: headers);
-      print("response status" + response.statusText.toString());
-      if (response.statusCode == HttpStatus.ok) {
-        // return WishModel.fromJson(response.body);
-        print('\n-- In Wish get service file:: wishGetService(){} : Block 1\n');
-        print(response.body);
-        print('-------------- End Block 1 ---------------');
-      } else {
-        print(
-            '\n-- In Wish get service file :: wishGetService(){} : Block 2\n');
-        print(response.statusCode);
-        print('-------------- End Block 2 ---------------');
-      }
-    } catch (e) {
-      print('\n-- In Wish get service file :: wishGetService(){} : Block 1\n');
-      print('Login Error :: ' + e.toString());
-      print('-------------- End Block 1 ---------------');
+    if (response.statusCode == HttpStatus.ok) {
+      List<dynamic> bdays = response.body;
+      return bdays.map((e) => WishModel.fromJson(e)).toList();
+    } else {
+      return null;
     }
-    // if (response.statusCode == HttpStatus.ok) {
-    //   return WishModel.fromJson(response.body);
-    // } else {
-    //   return null;
-    // }
   }
 }

56b4101f9e417d11602a68a99dfe8e5e0d23dc78
Samarth Khanna <samarth.khanna@peoplestrong.com>
2022-02-27 05:53:08 +0530

    HomeScreen: Major Changes regarding Navigation Flow

--- lib/screens/HomePage/HomePage.dart
+++ lib/screens/HomePage/HomePage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:workify/components/collapsed_side_menu.dart';
 import 'package:workify/components/side_menu.dart';
 import 'package:workify/controllers/UserController.dart';
+import 'package:workify/routes/home_routes.dart';
 import 'package:workify/screens/AttendancePage/AttendancePage.dart';
 import 'package:workify/screens/DashBoard.dart';
 import 'package:workify/screens/HomePage/HomePageController.dart';
@@ -15,78 +17,85 @@ class HomePage extends GetView<HomePageController> {
 
   HomePage({Key? key}) : super(key: key);
 
-  // final pages = [DashBoard(), ProfilePage(), AttendancePage(), LeavePage()];
   @override
   Widget build(BuildContext context) {
     DeviceSize device = DeviceSize();
     device.size = MediaQuery.of(context).size;
     final GlobalKey<ScaffoldState> _scaffoldkey = GlobalKey<ScaffoldState>();
 
-    return Obx(() => Scaffold(
-          key: _scaffoldkey,
-          //backgroundColor:  Theme.of(context).scaffoldBackgroundColor,
-          endDrawer: Drawer(),
-          body: Row(
-            children: [
-              SizedBox(
-                child: SideMenu(),
-                width: 236,
-              ),
-              Container(
-                width: 5,
-                height: double.infinity,
-                color: Colors.black12,
-              ),
-              Expanded(child: controller.getScreen()),
-            ],
+    return Scaffold(
+      key: _scaffoldkey,
+      //backgroundColor:  Theme.of(context).scaffoldBackgroundColor,
+      endDrawer: Drawer(),
+      body: Row(
+        children: [
+          Obx(() => SizedBox(
+                child: controller.sideMenuCollapsed.value
+                    ? CollapsedSideMenu()
+                    : SideMenu(),
+                width: controller.sideMenuCollapsed.value ? 84 : 236,
+              )),
+          Container(
+            width: 5,
+            height: double.infinity,
+            color: Colors.black12,
           ),
+          Expanded(
+              child: Navigator(
+            key: Get.nestedKey(1),
+            initialRoute: controller.pageName.value,
+            onGenerateRoute: HomeRouter.generateRoute,
+          ))
+          //Expanded(child: controller.getScreen()),
+        ],
+      ),
 
-          // Row(
-          //   children: [
-          //     Obx(() => AnimatedContainer(
-          //           duration: Duration(milliseconds: 200),
-          //           curve: isCollapsed.value ? Curves.linear : Curves.linear,
-          //           height: double.infinity,
-          //           width: isCollapsed.value ? 76 : 224,
-          //           child: MouseRegion(
-          //             onEnter: (event) => isCollapsed.value = false,
-          //             onExit: (event) => isCollapsed.value = true,
-          //             child: Material(
-          //               elevation: 100,
-          //               color: Theme.of(context).primaryColor,
-          //               borderOnForeground: true,
-          //               type: MaterialType.card,
-          //               shadowColor: Colors.black,
-          //               shape: RoundedRectangleBorder(
-          //                   borderRadius: BorderRadius.only(
-          //                       topRight: Radius.circular(6),
-          //                       bottomRight: Radius.circular(6))),
-          //               child: ListView(
-          //                 children: [
-          //                   Container(
-          //                     padding: EdgeInsets.symmetric(vertical: 24),
-          //                     height: bannerHeight,
-          //                     decoration: BoxDecoration(
-          //                         border: Border.fromBorderSide(
-          //                             BorderSide(color: Colors.black12))),
-          //                     child: buildHeader(),
-          //                   ),
-          //                   // Divider(
-          //                   //   color: kScaffoldColor,
-          //                   //   thickness: 1,
-          //                   // ),
-          //                   buildTiles()
-          //                 ],
-          //               ),
-          //             ),
-          //           ),
-          //         )),
-          //     const SizedBox(
-          //       width: 6,
-          //     ),
-          //     DashBoard(),
-          //   ],
-          // ),
-        ));
+      // Row(
+      //   children: [
+      //     Obx(() => AnimatedContainer(
+      //           duration: Duration(milliseconds: 200),
+      //           curve: isCollapsed.value ? Curves.linear : Curves.linear,
+      //           height: double.infinity,
+      //           width: isCollapsed.value ? 76 : 224,
+      //           child: MouseRegion(
+      //             onEnter: (event) => isCollapsed.value = false,
+      //             onExit: (event) => isCollapsed.value = true,
+      //             child: Material(
+      //               elevation: 100,
+      //               color: Theme.of(context).primaryColor,
+      //               borderOnForeground: true,
+      //               type: MaterialType.card,
+      //               shadowColor: Colors.black,
+      //               shape: RoundedRectangleBorder(
+      //                   borderRadius: BorderRadius.only(
+      //                       topRight: Radius.circular(6),
+      //                       bottomRight: Radius.circular(6))),
+      //               child: ListView(
+      //                 children: [
+      //                   Container(
+      //                     padding: EdgeInsets.symmetric(vertical: 24),
+      //                     height: bannerHeight,
+      //                     decoration: BoxDecoration(
+      //                         border: Border.fromBorderSide(
+      //                             BorderSide(color: Colors.black12))),
+      //                     child: buildHeader(),
+      //                   ),
+      //                   // Divider(
+      //                   //   color: kScaffoldColor,
+      //                   //   thickness: 1,
+      //                   // ),
+      //                   buildTiles()
+      //                 ],
+      //               ),
+      //             ),
+      //           ),
+      //         )),
+      //     const SizedBox(
+      //       width: 6,
+      //     ),
+      //     DashBoard(),
+      //   ],
+      // ),
+    );
   }
 }

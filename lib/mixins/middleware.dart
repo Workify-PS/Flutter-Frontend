85ee429f52b10728be1778f250eeb672edbba062
Samarth Khanna <samarth.khanna@peoplestrong.com>
2022-02-27 05:54:32 +0530

    Initialization of routes

--- lib/mixins/middleware.dart
+++ lib/mixins/middleware.dart
@@ -2,11 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:workify/controllers/AuthController.dart';
 import 'package:workify/mixins/cache.dart';
+import 'package:workify/screens/HomePage/HomePageController.dart';
 
 class AuthMiddlware extends GetMiddleware with CacheManager {
-  
-  RouteSettings? redirect(String? route) =>
-      (getToken() != null || route == "/auth")
-          ? null
-          : RouteSettings(name: "/auth");
+  @override
+  RouteSettings? redirect(String? route) {
+    if (true) //TODO: ADD LOGIC FOR CHECKING UNKNOWN ROUTE
+    {
+      if ((getToken() != null || route == "/auth")) {
+       final homeController = Get.put(HomePageController());
+         
+        homeController.pageName.value = route ?? "/home";
+        return null;
+      } else {
+        return RouteSettings(name: "/auth");
+      }
+    } else {
+      return null;
+    }
+  }
 }

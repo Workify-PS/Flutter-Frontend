e63b2313c7972ddd61e97dc77d88db9aab851dcb
Samarth Khanna <samarthkhanna24@.com>
2022-03-23 04:09:23 +0530

    tab button new

---
+++ lib/components/tab_button.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:workify/controllers/profile_widgets_controller.dart';
+import 'package:workify/utils/constants.dart';
+import 'package:workify/utils/theme.dart';
+
+class TabButton extends StatefulWidget {
+  // method with parameters in stateful widget
+  // final void Function(String) profileWidgetHandler;
+  // step to call such a method
+  // onpressed: () => profileWidgetHandler(profileDetails);
+
+  final String profileDetails;
+
+  TabButton({Key? key, required this.profileDetails}) : super(key: key);
+
+  @override
+  State<TabButton> createState() => _TabButtonState();
+}
+
+class _TabButtonState extends State<TabButton> {
+  final profileWidgetsController = Get.find<ProfileWidgetsController>();
+  RxBool isHover = false.obs;
+  @override
+  Widget build(BuildContext context) {
+    var button = widget.profileDetails == 'Basic Details'
+        ? profileWidgetsController.basicButton
+        : widget.profileDetails == 'Position Details'
+            ? profileWidgetsController.positionButton
+            : widget.profileDetails == 'Employment Details'
+                ? profileWidgetsController.employmentButton
+                : profileWidgetsController.modifyBasicButton;
+
+    return Obx(() => InkWell(
+          onTap: () {
+            profileWidgetsController.resetButtons();
+            profileWidgetsController.updateWidgetString(widget.profileDetails);
+            profileWidgetsController.setButton(str: widget.profileDetails);
+          },
+          focusColor: Colors.transparent,
+          hoverColor: Colors.transparent,
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+          onHover: (val) {
+            isHover.value = val;
+          },
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: kDefaultPadding / 1.2),
+            child: Center(
+              child: Text(
+                widget.profileDetails,
+                style: button.value
+                    ? Theme.of(context).textTheme.headline5!.copyWith(
+                          color: MyTheme().isDark(context)
+                              ? Colors.lightBlue
+                              : kPrimaryColor,
+                        )
+                    : Theme.of(context).textTheme.headline5!.copyWith(
+                        decoration: isHover.value
+                            ? TextDecoration.underline
+                            : TextDecoration.none),
+              ),
+            ),
+          ),
+        ));
+  }
+}
